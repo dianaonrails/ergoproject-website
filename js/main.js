a8e407e3075716373cf1612eaf88a8ada889bc7c
dianaonrails <d.r.carvalho89@gmail.com>
2016-09-26 20:56:35 +0100

    carousel issue
    carousel not going to beginning

--- js/main.js
+++ js/main.js
@@ -35,7 +35,8 @@ $(document).ready(function() {
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true,
-      autoPlay:true
+      autoPlay:true,
+      rewindNav:false
  
       // "singleItem:true" is a shortcut for:
       // items : 1, 
